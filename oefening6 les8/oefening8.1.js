668eb55225edb174d285ae1fc788b7cb2d6e4b1e
112ekki <eek_leuven@yahoo.com>
2020-11-12 21:22:58 +0100

    verwijder

--- oefening6 les8/oefening8.1.js
+++ oefening6 les8/oefening8.1.js
@@ -1,18 +1,19 @@
 
 
-    var characters   = ['ABC','DEF','GHI','JKL','MNO','PQR','ST','UV','WX','YZ','arf','uo', 'op', 'hsy', 'ui', 'fsd', 'tra', 'fdg', 'NO','PQR','ST','UV','eX','YtZ','af', 'bal']
+   let  lettergrepen  = ['Abc','Def','gih','jol','Mao','Pee','ST','UV','WX','YZ','arf','uo', 'op', 'hsy', 'ui', 'fod', 'tra', 'fog', 'NO','Per','Se','UV','eX','YeZ','af', 'bal']
+
+   let klas = [];
+   let result = '';
+
+   for ( let i = 0; i < 22; i++ ) {
+      let student = {};
+      student.naam = lettergrepen[Math.floor(Math.random() * lettergrepen.length)] +lettergrepen[Math.floor(Math.random() * lettergrepen.length)];
+      student.nummer = "NR-" + i;
+      klas.push(student);
+   }
+   console.log(klas);
  
-    var charactersLength = characters.length;
  
-    for ( var i = 0; i < length; i++ ) {
- 
-       result += characters.charAt(Math.floor(Math.random() * charactersLength));
- 
-    }
- 
-    return result;
- 
- }
  
   
 
