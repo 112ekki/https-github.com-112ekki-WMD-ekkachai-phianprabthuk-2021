a5feb9e29e27f2aba65663450ee9af577c62665c
112ekki <eek_leuven@yahoo.com>
2020-11-18 16:16:30 +0100

    niet helemaal af

---
+++ Oefening8 les10/oefening10-deel2.js
@@ -0,0 +1,42 @@
+
+
+
+
+const output = document.getElementById('output');
+  
+    document.getElementById('knop')
+        .addEventListener('click', (e) => {
+          haalDataOp();
+        },
+        true);
+  
+    function haalDataOp() {
+      fetch('https://matthiasdruwe.github.io/Web-MobileCursus/course/12.-JavaScript-04/examples/example12-01.json')
+          .then((response) => {
+            return response.json();
+          })
+          .then((data) => {
+            let tString = '';
+            data.forEach((e) => {
+              tString += `<section data-index="${e.index}">${e.name.first} ${e.name.last}</section>`;
+              tString += `<section data-index="${e.index}">${e.email}<br />${e.phone}</section>`;
+              
+
+            });
+            output.innerHTML = tString;
+          },
+          )
+          .then((err) => {
+            console.log(err);
+          });
+
+        }
+
+
+      
+
+
+
+
+
+
