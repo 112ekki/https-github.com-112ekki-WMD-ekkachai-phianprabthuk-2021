3096348d0f581662d7fdb6dfaad6b6149824dce8
112ekki <eek_leuven@yahoo.com>
2020-11-22 22:42:44 +0100

    final countdown

---
+++ Project/Movies_script.js
@@ -0,0 +1,70 @@
+const api_url= "https://api.themoviedb.org/3/discover/movie?api_key=194536ae9ff84c6efb6d222746610152&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1";
+const IMGPATH = "https://image.tmdb.org/t/p/w500/";
+const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?api_key=194536ae9ff84c6efb6d222746610152&language=en-US&query=";
+
+
+
+
+getMovies(api_url);
+
+async function getMovies(url) {
+    const resp = await fetch(url);
+    const respData = await resp.json();
+
+    console.log(respData);
+
+    showMovies(respData.results);
+}
+
+function showMovies(movies) {
+   
+    main.innerHTML = "";
+
+    movies.forEach((movie) => {
+        const { poster_path, title, vote_average, overview } = movie;
+
+        const movieEl = document.createElement("div");
+        movieEl.classList.add("movie");
+
+        movieEl.innerHTML = `
+            <img
+                src="${IMGPATH + poster_path}"
+                alt="${title}"
+            />
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span class="${getClassByRate(
+                    vote_average
+                )}">${vote_average}</span>
+            </div>
+            <div class="overview">
+                <h3>Overview:</h3>
+                ${overview}
+            </div>
+        `;
+
+        main.appendChild(movieEl);
+    });
+}
+
+function getClassByRate(vote) {
+    if (vote >= 8) {
+        return "green";
+    } else if (vote >= 5) {
+        return "orange";
+    } else {
+        return "red";
+    }
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const searchTerm = search.value;
+
+    if (searchTerm) {
+        getMovies(SEARCHAPI + searchTerm);
+
+        search.value = "";
+    }
+});
